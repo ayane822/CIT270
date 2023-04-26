e33db8ae54c5116c2f1d4e836c9340e8a77330c1
Aya Kurokawa <kur20002@byui.edu>
2023-04-26 13:41:47 -0600

    4/26 class

--- login.sh
+++ login.sh
@@ -1,1 +1,6 @@
 echo "Logging in"
+
+curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
+##POST means pushing something to website
+curl https://dev.stedi.me/validate/918c8229-fe39-4a70-8ec0-871b79d3f1d4 
+    
