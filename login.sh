b13a47aec537f1fc21c95486d8417fb8024f87c6
Aya Kurokawa <kur20002@byui.edu>
2023-05-05 13:36:14 -0600

    week3

--- login.sh
+++ login.sh
@@ -2,5 +2,9 @@ echo "Logging in"
 
 curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
 ##POST means pushing something to website
-curl https://dev.stedi.me/validate/918c8229-fe39-4a70-8ec0-871b79d3f1d4 
-    
+curl -v https://dev.stedi.me/validate/918c8229-fe39-4a70-8ec0-871b79d3f1d4 ## -> get the authentication token 
+## curl is to get client URL. port is either 443 or 80.
+## https -> encrypted web trafic -> port 443, http -> not encrypted port 80    
+##I get different token everytime I log in 
+
+
